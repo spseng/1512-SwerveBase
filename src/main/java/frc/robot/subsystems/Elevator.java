06c379f8cd326316ea757eb4f28191e784ffaff2
Motoki Nakajima <motokinakajima0912@gmail.com>
2025-02-15 10:14:42 -0500

    added elevator class

---
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
+
+public class Elevator extends SubsystemBase {
+    
+}
